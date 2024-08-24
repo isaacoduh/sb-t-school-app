36fcd9f30b39eb1f1a62027e2d117f1e1a7b0d8c
Isaac Oduh <ufuomaoduh@gmail.com>
2024-08-25 00:55:29 +0100

    savepoint: individual dashboards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,47 @@
+import Link from "next/link";
+import Image from "next/image";
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div>
+      <div className="">Homepage Links</div>
+      <br />
+      <div className="mt-4 text-sm">
+        <div className="flex flex-col gap-2">
+          <span className="hidden lg:block text-gray-400 font-light my-4 ml-2">
+            Dashboard Pages
+          </span>
+          <Link
+            href={"/admin"}
+            className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-[#EDF9FD]"
+          >
+            <Image src="/profile.png" alt="" width={28} height={20} />
+            <span className="hidden lg:block">Admin Dashboard</span>
+          </Link>
+          <Link
+            href={"/parent"}
+            className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-[#EDF9FD]"
+          >
+            <Image src="/profile.png" alt="" width={28} height={20} />
+            <span className="hidden lg:block">Parent Dashboard</span>
+          </Link>
+          <Link
+            href={"/student"}
+            className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-[#EDF9FD]"
+          >
+            <Image src="/profile.png" alt="" width={28} height={20} />
+            <span className="hidden lg:block">Student Dashboard</span>
+          </Link>
+          <Link
+            href={"/teacher"}
+            className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-[#EDF9FD]"
+          >
+            <Image src="/profile.png" alt="" width={28} height={20} />
+            <span className="hidden lg:block">Teacher Dashboard</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
