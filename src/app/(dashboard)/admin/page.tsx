82f9c4d0d141263170b2174009e96b3098344c86
Isaac Oduh <ufuomaoduh@gmail.com>
2024-08-24 16:52:34 +0100

    admin skeleton

---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,15 @@
+const AdminPage = () => {
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        <p>student</p>
+        <p>teacher</p>
+        <p>parent</p>
+        <p>staff</p>
+      </div>
+    </div>
+  );
+};
+
+export default AdminPage;
