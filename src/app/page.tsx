fea0962193dd6f305f103c3d81c7e556937d9069
Isaac Oduh <ufuomaoduh@gmail.com>
2024-08-25 01:15:13 +0100

    edit for small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Homepage = () => {
       <br />
       <div className="mt-4 text-sm">
         <div className="flex flex-col gap-2">
-          <span className="hidden lg:block text-gray-400 font-light my-4 ml-2">
+          <span className=" sm:block md:block lg:block text-gray-400 font-light my-4 ml-2">
             Dashboard Pages
           </span>
           <Link
@@ -15,28 +15,32 @@ const Homepage = () => {
             className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-[#EDF9FD]"
           >
             <Image src="/profile.png" alt="" width={28} height={20} />
-            <span className="hidden lg:block">Admin Dashboard</span>
+            <span className=" sm:block md:block lg:block">Admin Dashboard</span>
           </Link>
           <Link
             href={"/parent"}
             className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-[#EDF9FD]"
           >
             <Image src="/profile.png" alt="" width={28} height={20} />
-            <span className="hidden lg:block">Parent Dashboard</span>
+            <span className=" sm:block md:block lg:block">
+              Parent Dashboard
+            </span>
           </Link>
           <Link
             href={"/student"}
-            className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-[#EDF9FD]"
+            className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md sm:block md:block hover:bg-[#EDF9FD]"
           >
             <Image src="/profile.png" alt="" width={28} height={20} />
-            <span className="hidden lg:block">Student Dashboard</span>
+            <span className=" lg:block">Student Dashboard</span>
           </Link>
           <Link
             href={"/teacher"}
-            className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-[#EDF9FD]"
+            className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md sm:block md:block hover:bg-[#EDF9FD]"
           >
             <Image src="/profile.png" alt="" width={28} height={20} />
-            <span className="hidden lg:block">Teacher Dashboard</span>
+            <span className=" sm:block md:block lg:block">
+              Teacher Dashboard
+            </span>
           </Link>
         </div>
       </div>
